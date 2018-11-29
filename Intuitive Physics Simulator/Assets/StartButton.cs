863ea6a92875d868cee9c258494f2f9e132104b5
Gabe Schneider <gschneid@mit.edu>
2018-11-29 16:16:24 -0500

    Initial Menu and Scene

---
+++ Intuitive Physics Simulator/Assets/StartButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+	public void StartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
