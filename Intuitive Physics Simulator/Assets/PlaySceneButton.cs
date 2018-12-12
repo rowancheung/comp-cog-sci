b6196c950ecd5f4281f3ea07a2674528adf853b0
Gabe Schneider <gschneid@mit.edu>
2018-12-12 16:21:44 -0500

    Baseline Game

---
+++ Intuitive Physics Simulator/Assets/PlaySceneButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class PlaySceneButton : MonoBehaviour {
+
+    public Button button;
+    public bool sceneRan = false;
+
+    private void Start()
+    {
+        Physics.gravity = new Vector3(0, 0, 0);
+    }
+
+    public void startScene()
+    {
+
+        if (!sceneRan) {
+            Physics.gravity = new Vector3(0, -10, 0);
+
+            button.GetComponentInChildren<Text>().text = "Next Problem";
+            sceneRan = true;
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+    }
+}
